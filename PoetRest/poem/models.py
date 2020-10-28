7bc2ad376754f4e2360a6b53f563b52ad80c81fc
hyeongseok <aprilgom@github.com>
2020-10-28 13:55:46 +0000

    rest_update

--- PoetRest/poem/models.py
+++ PoetRest/poem/models.py
@@ -1,13 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
 
 # Create your models here.
-class User(models.Model):
-    email = models.CharField(max_length=40)
+class UserManager (BaseUserManager):
+    def create_user(self, email, name):
+        user = self.model(
+                email = email,
+                name = name
+        )
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    objects = UserManager()
+    email = models.CharField(max_length=40,unique=True)
+    USERNAME_FIELD = 'email'
     name = models.CharField(max_length=30)
+
 class Poem(models.Model):
     title = models.CharField(max_length=100)
     writer = models.CharField(max_length=30)
     content = models.TextField()
     likenum = models.IntegerField(default=0)
-    users = models.ManyToManyField(User)
+    #write_user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
 
--- PoetRest/poem/views.py
+++ PoetRest/poem/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 from django.http.response import JsonResponse
+from django.contrib import auth
+
+from django.db.models.aggregates import Count
 
 from rest_framework import viewsets
 from rest_framework import status
@@ -12,10 +15,34 @@ from google.oauth2 import id_token
 from google.auth.transport import requests
 
 from .serializers import UserSerializer,PoemSerializer
+from django.forms.models import model_to_dict
+from django.core import serializers
 
 from .models import Poem
+from .models import User
+
+import random
 
 class PoemViewSet(viewsets.ModelViewSet):
     queryset = Poem.objects.all()
     serializer_class = PoemSerializer
     #permission_classes = [permissions.IsAuthenticated]
+
+@api_view(['GET'])
+def Recommend(request):
+    count = Poem.objects.aggregate(count=Count('id'))['count']
+    rand_i  = random.randrange(0,count)
+    randomPoem = Poem.objects.all()[rand_i]
+    data = serializers.serialize("json",[randomPoem],ensure_ascii=False)
+    return JsonResponse(data,safe=False,status = status.HTTP_201_CREATED)
+    
+@api_view(['POST'])
+def ManualSignUp(request):
+    req_data = JSONParser().parse(request)
+    email = req_data['email']
+    name = req_data['name']
+    User.objects.create_user(email = email,name=name)
+    auth.login(request,user)
+
+    
+
