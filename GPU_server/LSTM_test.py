cf1163d43022f5d97284c2a39ee4fcb52b99c69f
grr1203 <44797127+grr1203@users.noreply.github.com>
2020-11-06 21:12:16 +0900

    Update LSTM_test.py

--- GPU_server/LSTM_test.py
+++ GPU_server/LSTM_test.py
@@ -1,8 +1,6 @@
 import numpy as np
 import pandas as pd
 import pickle
-%matplotlib inline
-import matplotlib.pyplot as plt
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.utils import to_categorical
