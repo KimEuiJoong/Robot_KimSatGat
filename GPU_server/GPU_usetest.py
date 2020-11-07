f0fcb57b8509ed4c32672d770b85524526811799
grr1203 <44797127+grr1203@users.noreply.github.com>
2020-11-08 04:31:48 +0900

    Update GPU_usetest.py

--- GPU_server/GPU_usetest.py
+++ GPU_server/GPU_usetest.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from tensorflow.python.client import device_lib
 
 tf.debugging.set_log_device_placement(True)
 
@@ -7,3 +8,5 @@ b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
 c = tf.matmul(a, b)
 
 print(c)
+
+print(device_lib.list_local_devices())
