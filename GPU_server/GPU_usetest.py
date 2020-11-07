3ee5a2e78b452b169ed65d847457e4bddfd29519
grr1203 <44797127+grr1203@users.noreply.github.com>
2020-11-08 04:39:44 +0900

    Update GPU_usetest.py

--- GPU_server/GPU_usetest.py
+++ GPU_server/GPU_usetest.py
@@ -1,12 +1,14 @@
 import tensorflow as tf
 from tensorflow.python.client import device_lib
-
 tf.debugging.set_log_device_placement(True)
 
-a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
-b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
-c = tf.matmul(a, b)
-
-print(c)
-
 print(device_lib.list_local_devices())
+
+try:
+  with tf.device('/device:XLA_GPU:0'):
+    a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+    b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
+    c = tf.matmul(a, b)
+    print('run end')
+except RuntimeError as e: #유효하지 않은 GPU 장치를 명시
+  print(e)
